fd97b6982c41460bfeb0b8a30bee222ae4beef95
lavanyamano3 <LAVANYAMANO@outlook.com>
2017-02-06 15:03:07 -0500

    explicit converter

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace csharp
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			Console.WriteLine("second Commit");
+			float f = 123.45f;
+			int i = Convert.ToInt32(f);
+			int j = (int)f;
+
+			Console.WriteLine("convert to int:f  {0}",i);
+			Console.WriteLine("explicit int converter:   {0}", j);
 		}
 	}
 }
