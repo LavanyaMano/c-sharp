c9d6f45c9f49badae4bbc0ec548cb30b33203c27
lavanyamano3 <LAVANYAMANO@outlook.com>
2017-02-14 11:23:36 -0500

    Parameter types

--- Program.cs
+++ Program.cs
@@ -4,29 +4,58 @@ namespace csharp
 {
 	class MainClass
 	{
-		public int factorial(int num)
+		public void swap(int x, int y)
 		{
-			// recursive method call
+			int temp;
+			temp = x;
+			x = y;
+			y = temp;
+		}
+		public void swapRef(ref int x, ref int y)
+		{
+			int temp;
+			temp = x;
+			x = y;
+			y = temp;
 
-			int result;
+		}
 
-			if (num == 1)
-			{
-				return 1;
-			}
-			else
-			{
-				result = factorial(num - 1) * num;
-				return result;
-			}
+		public void getValue(out int x)
+		{
+			int temp = 5;
+			x = temp;
 		}
 		static void Main(string[] args)
 		{
 			MainClass m = new MainClass();
 
-			// calling the factorial method
+			int a = 10;
+			int b = 20;
+			int c = 30;
+			int d = 40;
 
-			Console.WriteLine("Factorial of 6 is {0}", m.factorial(6));
+			Console.WriteLine("Before swaping, value of a : {0}", a);
+			Console.WriteLine("Before swaping, value of b : {0}", b);
+			Console.WriteLine("Before swaping, value of c : {0}", c);
+			Console.WriteLine("Before swaping, value of d : {0}", d);
+			// Passing parameters by value (copies the value of an arg)
+
+			m.swap(a, b);
+
+			// Reference Parameters(copies the reference to the memory location of an arg)
+
+			m.swapRef(ref c, ref d);
+
+			Console.WriteLine("After swaping, value of a : {0}", a);
+			Console.WriteLine("After swaping, value of b : {0}", b);
+			Console.WriteLine("After swaping, value of c : {0}", c);
+			Console.WriteLine("After swaping, value of d : {0}", d);
+
+			//output parameter
+
+			m.getValue(out a);
+
+			Console.WriteLine("After getvalue method call, value of a is {0}", a);
 		}
 	}
 
