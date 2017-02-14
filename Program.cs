a25b3dbc2b3586865fa8b86ba7a77e0fbde8e96e
lavanyamano3 <LAVANYAMANO@outlook.com>
2017-02-14 11:05:57 -0500

    Recursive Method call

--- Program.cs
+++ Program.cs
@@ -4,62 +4,30 @@ namespace csharp
 {
 	class MainClass
 	{
-		//Encapsulation: Enclosing one or more items within in a physical or logical package.
-		//Its done using access specifiers.(Public, Private, Protected, Internal, Protected internal)
-
-
-	}
-	class Rectangle
-	{
-		//member variables public
-		public double length;
-		public double width;
-
-		public double GetArea()
+		public int factorial(int num)
 		{
-			return length * width;
+			// recursive method call
+
+			int result;
+
+			if (num == 1)
+			{
+				return 1;
+			}
+			else
+			{
+				result = factorial(num - 1) * num;
+				return result;
+			}
 		}
-
-		public void Display()
-		{
-			Console.WriteLine("Length: {0}", length);
-			Console.WriteLine("Width:{0}", width);
-			Console.WriteLine("Area:{0}", GetArea());
-		}
-
-		//private variables
-		private double length1;
-		private double width1;
-
-		public void Acceptdetails()
-		{
-			Console.WriteLine("Enter length : ");
-			length1 = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Enter width: ");
-			width1 = Convert.ToDouble(Console.ReadLine());
-		}
-
-		public void Display1()
-		{
-			Console.WriteLine("Private variable length1 is {0}",length1);
-			Console.WriteLine("Private variable width1 is {0}", width1);
-		}
-
-	}// end class Rectangle
-	class ExecuteRectangle
-	{
 		static void Main(string[] args)
 		{
-			Rectangle r = new Rectangle();
+			MainClass m = new MainClass();
 
-			r.length = 4.5;
-			r.width = 3.5;
-			r.Display();
-			Console.WriteLine("length1 and width1 are private. Can't call here");
-			r.Acceptdetails();
-			r.Display1();
+			// calling the factorial method
+
+			Console.WriteLine("Factorial of 6 is {0}", m.factorial(6));
 		}
-
 	}
 
 }
